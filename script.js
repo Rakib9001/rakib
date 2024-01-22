d24d486bcf3e77d2fbbda07548cd8cfbf256a8e4
Rakib9001 <100214638+Rakib9001@users.noreply.github.com>
2024-01-22 21:11:44 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed((".skills"),{
+    strings : ["Frontend developer","Web developer","Ui designer"],
+    typeSpeed : 150,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
+var typed = new Typed((".rakib1"),{
+    strings : ["HI,","I am Rakibul islam",],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
